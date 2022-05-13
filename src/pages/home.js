5eb21e82251c1557ac597760c39bf6b02b178cf8
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-05-13 17:26:27 -0600

    components

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,38 +1,9 @@
-import LightModeIcon from "../assets/moon_light_icon.svg";
-import DarkModeIcon from "../assets/moon_icon.svg";
-import filterLightIcon from "../assets/filter_light.svg";
-import filterDarkIcon from "../assets/filter_dark.svg";
-import searchIcon from "../assets/search_icon.svg";
+import Navigation from "../components/navigation";
 
 const Home = () => {
   return (
     <>
-      <div className="navigation-wrapper">
-        <nav className="navigation">
-          <h1 className="brand">devJobs</h1>
-          <div className="toggle-wrapper">
-            <img src={LightModeIcon} alt="dark mode toggle icon" />
-            <div className="toggle-container">
-              <label htmlFor="toggle">
-                <input type="checkbox" id="toggle" name="toggle" />
-                <span className="toggle-container"></span>
-              </label>
-            </div>
-            <img src={DarkModeIcon} alt="dark mode toggle icon" />
-          </div>
-        </nav>
-        <div className="search-container">
-          <div className="mobile-search">
-            <input type="text" placeholder="Filter by title" />
-            <button className="form-icon">
-              <img src={filterLightIcon} alt="filter" />
-            </button>
-            <button className="mobile-submit">
-              <img src={searchIcon} alt="search icon" />
-            </button>
-          </div>
-        </div>
-      </div>
+      <Navigation />
 
       <main className="main"></main>
     </>
