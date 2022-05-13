34df222baa4a9db72af935807e6c158ebaf1f3ac
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-05-13 17:58:29 -0600

    adding theme context to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import { themeContext } from "./context/themeContext";
+import { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("light");
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <themeContext.Provider value={{ theme, setTheme }}>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </themeContext.Provider>
     </Router>
   );
 }
