447d88dee33ba80813e41595339345dc79d73352
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-05-17 19:00:09 -0600

    added styling and transition

--- src/components/customCheckbox.js
+++ src/components/customCheckbox.js
@@ -1,6 +1,6 @@
 const CustomCheckbox = ({ htmlForName }) => {
   return (
-    <label className="custom-label wrapper" htmlFor={htmlForName}>
+    <label className="custom-label-wrapper" htmlFor={htmlForName}>
       <input type={"checkbox"} className="default-checkbox" id={htmlForName} />
       <span className="custom-checkmark" />
       Full Time only
